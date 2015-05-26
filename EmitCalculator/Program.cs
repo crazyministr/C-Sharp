6cf828d745f7ee66105eebc8b8158bd8f97c2b31
crazyministr <crazyministr@gmail.com>
2015-05-26 21:21:11 +0400

    fifth task complete

--- EmitCalculator/Program.cs
+++ EmitCalculator/Program.cs
@@ -5,43 +5,56 @@ namespace FifthTask
 {
     class EmitCalculator
     {
-        private string expr;
+        private string[] tokens;
         private DynamicMethod dyn;
         private ILGenerator gen;
         private Type[] argvTypes = {typeof(int), typeof(int), typeof(int), typeof(int), typeof(int)};
-        public Func<int, int, int, int, int> Calculation;
-        // public delegate int Calculation(int a, int b, int c, int d);
+        // private Func<int, int, int, int, int> Calculation;
+        private delegate int Calculation(int a, int b, int c, int d, int ret);
 
-        public EmitCalculator(string expr)
+        public EmitCalculator(string expr, int a, int b, int c, int d)
         {
             this.dyn = new DynamicMethod("Calculate",  // name of dinamic method
                                          typeof(int),  // return type
                                          argvTypes,    // parameter types
-                                         typeof(int).Module); // the type with which the dynamic method is logically associated
+                                         typeof(int)); // the type with which the dynamic method is logically associated
+
+            this.tokens = expr.Split(new Char[] {' '});
             this.gen = dyn.GetILGenerator();
-            gen.Emit(OpCodes.Ldloc, this.expr);
-            // this.GenerateCode();
+            this.GenerateCode();
             Calculation calculation = (Calculation) dyn.CreateDelegate(typeof(Calculation));
+            Console.WriteLine(calculation(a, b, c, d, 0));
         }
 
-        // private void GenerateCode()
-        // {
-        //     Label beginLoop = gen.DefineLabel();
-        //     Label endLoop = gen.DefineLabel();
-        //     gen.Emit(OpCodes.Ldc_I4, 13);
-        //     // gen.MarkLabel(beginLoop);
-
-        //     // gen.MarkLabel(endLoop);
-        //     gen.Emit(OpCodes.Ret);
-        // }
+        private void GenerateCode()
+        {
+            foreach (string token in this.tokens)
+            {
+                if (token.Equals("+"))
+                    gen.Emit(OpCodes.Add);
+                else if (token.Equals("*"))
+                    gen.Emit(OpCodes.Mul);
+                else
+                {
+                    if (token.Equals("a"))
+                        gen.Emit(OpCodes.Ldarg_0);
+                    else if (token.Equals("b"))
+                        gen.Emit(OpCodes.Ldarg_1);
+                    else if (token.Equals("c"))
+                        gen.Emit(OpCodes.Ldarg_2);
+                    else if (token.Equals("d"))
+                        gen.Emit(OpCodes.Ldarg_3);
+                }
+            }
+            gen.Emit(OpCodes.Ret);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            EmitCalculator calculator = new EmitCalculator("a b * c + d +");
-            calculator.calculation(2, 3, 4, 3);
+            EmitCalculator calculator = new EmitCalculator("a b * c + d +", 2, 3, 4, 3);
         }
     }
 }
