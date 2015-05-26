ce9759c5a97d4c10da12d043ff435ff39ddbf220
crazyministr <crazyministr@gmail.com>
2015-05-26 20:25:14 +0400

    almost, almost

--- EmitCalculator/Program.cs
+++ EmitCalculator/Program.cs
@@ -8,40 +8,40 @@ namespace FifthTask
         private string expr;
         private DynamicMethod dyn;
         private ILGenerator gen;
-        private Type[] argvTypes = {typeof(int), typeof(int), typeof(int), typeof(int)};
-        private Func<int, int, int, int, int> Calculation;
+        private Type[] argvTypes = {typeof(int), typeof(int), typeof(int), typeof(int), typeof(int)};
+        public Func<int, int, int, int, int> Calculation;
+        // public delegate int Calculation(int a, int b, int c, int d);
 
         public EmitCalculator(string expr)
         {
-            this.expr = expr;
             this.dyn = new DynamicMethod("Calculate",  // name of dinamic method
                                          typeof(int),  // return type
                                          argvTypes,    // parameter types
-                                         typeof(int)); // the type with which the dynamic method is logically associated
+                                         typeof(int).Module); // the type with which the dynamic method is logically associated
             this.gen = dyn.GetILGenerator();
-            this.GenerateCode();
-            this.Calculation = dyn.CreateDelegate(typeof(Calculation));
+            gen.Emit(OpCodes.Ldloc, this.expr);
+            // this.GenerateCode();
+            Calculation calculation = (Calculation) dyn.CreateDelegate(typeof(Calculation));
         }
 
-        public GenerateCode()
-        {
-            Label beginLoop = gen.DefineLabel();
-            Label endLoop = gen.DefineLabel();
-            
-            gen.Emit(OpCodes.Ldstr, this.expr);
-            gen.MarkLabel(beginLoop);
+        // private void GenerateCode()
+        // {
+        //     Label beginLoop = gen.DefineLabel();
+        //     Label endLoop = gen.DefineLabel();
+        //     gen.Emit(OpCodes.Ldc_I4, 13);
+        //     // gen.MarkLabel(beginLoop);
 
-            gen.MarkLabel(endLoop);
-            gen.Emit(OpCodes.Ret);
-        }
+        //     // gen.MarkLabel(endLoop);
+        //     gen.Emit(OpCodes.Ret);
+        // }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var calculator = EmitCalculator("a b * c + d +");
-            calculator.Calculation(2, 3, 4, 3);
+            EmitCalculator calculator = new EmitCalculator("a b * c + d +");
+            calculator.calculation(2, 3, 4, 3);
         }
     }
 }
